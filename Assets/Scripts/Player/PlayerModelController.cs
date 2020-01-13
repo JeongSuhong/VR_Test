fd9bdb287752c4622fe9998055be3cce8ea8460a
JeongSuhong <ghjkdfe@gmail.com>
2020-01-13 21:25:01 +0900

    VR Player Model 움직이게 하는 스크립트 제작중

---
+++ Assets/Scripts/Player/PlayerModelController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerModelController : MonoBehaviour
+{
+    public Transform HeadCamera;
+    public Transform HeadModel;
+
+
+    private void Update()
+    {
+        Vector3 rot = HeadModel.rotation.eulerAngles;
+        rot += HeadCamera.rotation.eulerAngles * 0.1f;
+
+        HeadModel.rotation = Quaternion.Euler(rot);
+    }
+}
